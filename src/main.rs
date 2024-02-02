2be6255d6ea408ea90cab8bf50e8f0965cb2b6aa
0xumarkhatab <umarkhatabfrl@gmail.com>
2024-02-02 16:50:45 +0500

    learned basic programming concepts in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::any::type_name;
+
+fn variables() {
+    let mut x: i32 = 10;
+    println!("value of x {x}");
+    x = 12;
+    println!("value of x {x}");
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (a, b, _) = tup;
+    print!("{a} {b}");
+    // let tup1 = ();
+    // print!({ type_name(tup1) + "" });
+}
 fn main() {
-    println!("Hello, world!");
+    variables()
 }
